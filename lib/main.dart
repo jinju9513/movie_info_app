410154aece1d79dbc68bb6b991cfc1902de8c29b
jinju9513 <wjswlswn99@naver.com>
2025-05-13 20:40:02 +0900

    feat : provider 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movie_info_app/data/datasoures/movie_data_source_impl.dart';
+import 'package:movie_info_app/data/repositories/movie_repository_impl.dart';
+import 'package:movie_info_app/domain/usecases/fetch_now_playing_movies_usecase.dart';
+import 'package:movie_info_app/domain/usecases/fetch_popular_movies_usecase.dart';
+import 'package:movie_info_app/domain/usecases/fetch_top_rated_movies_usecase.dart';
+import 'package:movie_info_app/domain/usecases/fetch_upcoming_movies_usecase.dart';
 import 'package:movie_info_app/presentation/pages/home_page.dart';
+import 'package:movie_info_app/presentation/viewmodels/movie_view_model.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  final repository = MovieRepositoryImpl(MovieDataSourceImpl());
+
+  runApp(
+    MultiProvider(
+      providers: [ 
+        ChangeNotifierProvider(
+          create:
+              (_) => MovieViewModel(
+                fetchPopularMovies: FetchPopularMoviesUseCase(repository),
+                fetchNowPlayingMovies: FetchNowPlayingMoviesUseCase(repository),
+                fetchTopRatedMovies: FetchTopRatedMoviesUseCase(repository),
+                fetchUpcomingMovies: FetchUpcomingMoviesUseCase(repository),
+              )..loadMovies(), // 앱 시작하자 마자 데이터 로드
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,8 +39,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     home: HomePage(),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
